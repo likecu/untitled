2ba43e95ff55d347b941201f78107fa3d0d2e95c
likecu <254067848@qq.com>
2023-07-02 20:45:24 +0800

    #comment static method

--- src/BasicStudy.java
+++ src/BasicStudy.java
@@ -1,2 +1,17 @@
+
+/**
+ * @Description: 自由块
+ * @Author: likecu
+ * @Date: 2023/7/2
+ */
 public class BasicStudy {
+
+    static int x=10;
+    static { x/=3;}
+    static { x+=5;}
+
+
+    public static void main(String[] args) {
+        System.out.println(x); // 5
+    }
 }
