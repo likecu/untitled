c67789baa8b32ec3e35916aeb6f8174fbc30326e
likecu <254067848@qq.com>
2023-07-02 22:02:39 +0800

    #comment 不能初始化就使用

--- src/BasicStudy.java
+++ src/BasicStudy.java
@@ -13,5 +13,8 @@ public class BasicStudy {
 
     public static void main(String[] args) {
         System.out.println(x); // 5
+        // 不能不初始化就使用
+        Integer k = null;
+        System.out.println((Object) null);
     }
 }
